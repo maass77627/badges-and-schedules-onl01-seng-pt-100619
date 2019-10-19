3dd2fab00e0843814efc0af65a67edf6ed5983be
kelly r maass <kmaass77627@gmail.com>
2019-10-19 22:17:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,5 +18,15 @@ end
 end 
   
 def printer(attendees)
-  batch_badge_creator(attendees).each do ||
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end 
+  end
+  
+  
+  
   
