f6bac6d6486dd1f3172751e903fde94464c5860e
kelly r maass <kmaass77627@gmail.com>
2019-10-19 21:13:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,20 +1,15 @@
 # Write your code here.
 def badge_maker(name)
- puts  "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
  end
  
- 
- 
- batch_badge_creator(array)
-  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-  counter = 0
-  array.each do |name|
-    puts "Hello, my name is #{name}."
-    counter += 1
+ def batch_badge_creator(attendees)
+ attendees.collect do |name|
+   badge_maker(name)
   end
 end
 
-def assign_rooms
+def assign_rooms(attendees)
   
   #"Hello, \_\_\_\_\_! You'll be assigned to room \_\_\_\_\_!"
   
