20d7e7ac223dfc06f47723d0305ffbf6d5cd1e68
kelly r maass <kmaass77627@gmail.com>
2019-10-19 21:08:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def badge_maker(name)
+ puts  "Hello, my name is #{name}."
+ end
+ 
+ 
+ 
+ batch_badge_creator(array)
+  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  counter = 0
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+    counter += 1
+  end
+end
+
+def assign_rooms
+  
+  #"Hello, \_\_\_\_\_! You'll be assigned to room \_\_\_\_\_!"
+  
+ # printer
